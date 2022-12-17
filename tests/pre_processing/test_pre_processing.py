003244a67cb1b8b8c2f450ff550992ce8d3a7a82
CatalinaOdizzio <catalina.odizzio@bse.eu>
2022-12-17 19:29:16 +0100

    pre_processing test

--- tests/pre_processing/test_pre_processing.py
+++ tests/pre_processing/test_pre_processing.py
@@ -1,17 +1,22 @@
 import unittest
 import pytest
 from pandas.testing import assert_frame_equal
-#%%
-from lumpia.load_data.load_data import read_data
-#%%
+from lumpia.pre_processing.pre_processing import drop_nan
+from lumpia.pre_processing.pre_processing import fill_mean
 import pandas as pd
-from datetime import datetime
-class tests(unittest.TestCase):
 
-# tests for preprocessing
-        def test_read_data(self):
-                ans = pd.read_csv("churn_data.csv", sep=';')
-                res = read_data("churn_data.csv")
-                assert_frame_equal(res, ans)
+class Testdatacleanup(unittest.TestCase):
+    
+    def test_drop_nan(self):
+        column = ['col'] 
+        df = pd.DataFrame({'col': [0,5,10, np.nan]})
+        output = drop_nan(df, column)
+        expected_output = pd.DataFrame({'col': [0,5,10]})
+        assert_frame_equal(output,expected_output,check_dtype=False)
         
-# todo: tests for feature computation
+    def test_fill_mean(self):
+        df = pd.DataFrame({'col': [0,5,10, np.nan]})
+        column = ['col'] 
+        output = fill_mean(df,column)
+        expected_output = pd.DataFrame({'col': [0,5,10,5]})
+        assert_frame_equal(output,expected_output,check_dtype=False)
