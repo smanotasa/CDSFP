f9fa0ec2205974331113274ad20486cde28a38d7
smanotasa <santiago.manotasarroyave@bse.eu>
2022-12-17 18:42:08 +0100

    Jupyter Notebook

--- src/lumpia/model/model.py
+++ src/lumpia/model/model.py
@@ -6,11 +6,8 @@ from sklearn.ensemble import RandomForestClassifier
 import warnings
 warnings.filterwarnings('ignore')
 
-def train_model(df: pd.DataFrame, feature_cols: list, target_cols: list):
+def train_model(X_train, X_test, y_train, y_test, feature_cols: list, target_cols: list):
 
-    X = df[feature_cols]
-    y = df[target_cols]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2, stratify=y)
     rf_clf = RandomForestClassifier(n_estimators=500, random_state=10)
     rf_clf.fit(X_train, y_train)
     
