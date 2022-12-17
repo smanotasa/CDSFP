c599d2d43056ffb740a239c0a34ec74d41ca71ae
agospissinis <117668638+agospissinis@users.noreply.github.com>
2022-12-17 17:42:27 +0100

    Update load_data.py

--- src/lumpia/load_data/load_data.py
+++ src/lumpia/load_data/load_data.py
@@ -1,19 +1,14 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
 
-class dataset:
+def read_data(path: str):
+    '''
+    Read database.
     
-    def __init__(self, file):
-        self.file = file
-        self.loadnsplitter(self.file)
-            
-    def loadnsplitter(self, file):
-        try:
-            self.df = pd.read_csv(file, index_col=0, decimal='.')
-        except NameError:
-            print('There is no such file')
-        except FileNotFoundError:
-            print('There is no such file')
-            
-        self.train, self.test = train_test_split(self.df, random_state = 420, train_size = .80)
-        return self.train, self.test
+    Parameters: 
+        path: path to data
+        
+    Returns:
+        df (dataframe): database in pandas format
+    '''
+    df = pd.read_csv(path, index_col=0)
+    return df
