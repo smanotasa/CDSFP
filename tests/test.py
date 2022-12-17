51e24fffdada794f5c33d49574a31e812a39e865
yzse <54381977+yzse@users.noreply.github.com>
2022-12-17 16:21:45 +0100

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -8,4 +8,10 @@ import pandas as pd
 from datetime import datetime
 class tests(unittest.TestCase):
 
-# todo: create tests
+# tests for preprocessing
+def test_read_data(self):
+        ans = pd.read_csv("churn_data.csv")
+        res = read_data("churn_data.csv")
+        assert_frame_equal(res, ans)
+        
+# todo: tests for feature computation
