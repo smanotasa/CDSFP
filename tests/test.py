2fbe155ce5e648a382c0d3c1782c5fd8fc044955
yzse <54381977+yzse@users.noreply.github.com>
2022-12-17 16:22:15 +0100

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -2,7 +2,7 @@ import unittest
 import pytest
 from pandas.testing import assert_frame_equal
 #%%
-from src.hw5.hw5 import *
+from src.lumpia import *
 #%%
 import pandas as pd
 from datetime import datetime
