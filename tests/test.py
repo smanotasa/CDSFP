44606c6901af1e6a6b94b5b187f7bd56e3f347cd
yzse <54381977+yzse@users.noreply.github.com>
2022-12-17 16:18:25 +0100

    initialize test.py

---
+++ tests/test.py
@@ -0,0 +1,11 @@
+import unittest
+import pytest
+from pandas.testing import assert_frame_equal
+#%%
+from src.hw5.hw5 import *
+#%%
+import pandas as pd
+from datetime import datetime
+class tests(unittest.TestCase):
+
+# todo: create tests
