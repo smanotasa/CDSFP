e8a8be7869d5bfbcfb96638f7d846167bb121e51
CatalinaOdizzio <catalina.odizzio@bse.eu>
2022-12-17 17:32:31 +0100

    Test

---
+++ tests/features/test_features.py
@@ -0,0 +1,14 @@
+
+from lumpia.features.features import gen_dummies
+import unittest
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+class Testfeatures(unittest.TestCase):
+    
+    def test_gen_dummy(self):
+        data = pd.DataFrame({'col': ['Caucasian','Hispanic','Caucasian','Asian']})
+        column = ['col'] 
+        output = gen_dummies(data,column)
+        expected_output = pd.DataFrame({'col_Caucasian': [1,0,1,0], 'col_Hispanic': [0,1,0,0]})
+        assert_frame_equal(output,expected_output,check_dtype=False)
